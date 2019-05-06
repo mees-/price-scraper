217faf10d19b8b84c15696992525e376599e63b3
Mees van Dijk <mees@mees.io>
2019-05-06 14:06:48 +0200

    finished script (?)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer')
 const delay = require('delay')
 const PQueue = require('p-queue')
+const fs = require('fs-extra')
 
 const tryRetry = async (fn, maxTries = 10000) => {
   let result
@@ -20,52 +21,45 @@ const tryRetry = async (fn, maxTries = 10000) => {
 }
 
 const getPrice = async (licenseNumber, kilometers, page) => {
-  try {
-    await page.goto('https://www.anwb.nl/auto/koerslijst#/kenteken')
+  await page.goto('https://www.anwb.nl/auto/koerslijst#/kenteken')
 
-    await tryRetry(() => page.click('#licenseplate'))
-    await page.keyboard.type(licenseNumber)
-    await page.click('#kilometers')
-    await delay(100)
-    await page.keyboard.type(kilometers)
+  await tryRetry(() => page.click('#licenseplate'))
+  await page.keyboard.type(licenseNumber)
+  await page.click('#kilometers')
+  await delay(100)
+  await page.keyboard.type(kilometers)
 
-    await page.click('#kenteken-button-continue')
-    await tryRetry(
-      () =>
-        page.click(
-          'body > main > section:nth-child(2) > div > div > section > div > div > div.ng-scope > div > form > div > fieldset > div > label',
-        ),
-      100,
+  await page.click('#kenteken-button-continue')
+  await tryRetry(
+    () =>
+      page.click(
+        'body > main > section:nth-child(2) > div > div > section > div > div > div.ng-scope > div > form > div > fieldset > div > label',
+      ),
+    100,
+  )
+
+  await page.click('#uitvoering-button-continue')
+
+  await tryRetry(() => page.click('#opties-button-continue'))
+
+  const el = await tryRetry(async () => {
+    const res = await page.$(
+      'body > main > section:nth-child(2) > div > div > section > div > div > div.ng-scope > div > form > div > div.panel-form-response.ng-scope > div:nth-child(1) > ul > li:nth-child(4) > span:nth-child(2)',
     )
-
-    await page.click('#uitvoering-button-continue')
-
-    await tryRetry(() => page.click('#opties-button-continue'))
-
-    const el = await tryRetry(async () => {
-      const res = await page.$(
-        'body > main > section:nth-child(2) > div > div > section > div > div > div.ng-scope > div > form > div > div.panel-form-response.ng-scope > div:nth-child(1) > ul > li:nth-child(4) > span:nth-child(2)',
-      )
-      if (res == null) {
-        throw Error('Incorrect element')
-      } else {
-        return res
-      }
-    })
-    const price = await page.evaluate(element => element.innerText, el)
-
-    await page.close()
-    return price
-  } catch (e) {
-    await page.close()
-    throw e
-  }
+    if (res == null) {
+      throw Error('Incorrect element')
+    } else {
+      return res
+    }
+  })
+  const price = await page.evaluate(element => element.innerText, el)
+  return price
 }
 
-const KM_PER_YEAR = 1200
+const KM_PER_YEAR = 3000
 
 const input = require('./data.json')
-const output = []
+let done = 0
 
 ;(async () => {
   const browser = await puppeteer.launch({
@@ -73,102 +67,70 @@ const output = []
     defaultViewport: { width: 1920, height: 1080 },
   })
 
-  const queue = new PQueue({ concurrency: 5 })
-
+  const queue = new PQueue({ concurrency: 10 })
+  const writeStream = fs.createWriteStream('./data2.json')
   input
+    .map(row => ({
+      ...row,
+      years:
+        (new Date() -
+          new Date(
+            `${row.Datumeerstetoelating.slice(
+              0,
+              4,
+            )}-${row.Datumeerstetoelating.slice(
+              4,
+              6,
+            )}-${row.Datumeerstetoelating.slice(6)}`,
+          )) /
+        (1000 * 60 * 60 * 24 * 365),
+    }))
+    .filter(row => row.years < 15)
     .map(row => async () => {
-      output.push(await addPrice(row, browser))
-      console.log(`progress: ${(output.length / input.length) * 100}%`)
+      writeStream.write(`${JSON.stringify(await addPrice(row, browser))},`)
+      done++
+      console.log(`progress: ${(done / input.length) * 100}%`)
     })
     .forEach(fn => queue.add(fn))
 
-  // for (const entry of input) {
-  //   for (const page of await browser.pages()) {
-  //     await page.close()
-  //   }
-
-  //   const years =
-  //     (new Date() -
-  //       new Date(
-  //         `${entry.Datumeerstetoelating.slice(
-  //           0,
-  //           4,
-  //         )}-${entry.Datumeerstetoelating.slice(
-  //           4,
-  //           6,
-  //         )}-${entry.Datumeerstetoelating.slice(6)}`,
-  //       )) /
-  //     (1000 * 60 * 60 * 24 * 365)
-
-  //   if (years >= 15) {
-  //     console.log(`Skipping ${entry.Kenteken}, it's too old (${years} old)`)
-  //     output.push({ ...entry, prijs: '-1' })
-  //     continue
-  //   }
-  //   const kilometers = KM_PER_YEAR * years
-
-  //   try {
-  //     const price = await getPrice(
-  //       entry.Kenteken,
-  //       kilometers.toString().split('.')[0],
-  //       browser,
-  //     )
-  //     console.log(`prijs voor ${entry.Kenteken}: ${price}`)
-  //     console.log(`progress: ${(output.length / input.length) * 100}%`)
-  //     output.push({ ...entry, prijs: price })
-  //   } catch (e) {
-  //     console.error(
-  //       `failed to get price for ${entry.Kenteken}, kilometers: ${
-  //         kilometers.toString().split('.')[0]
-  //       }`,
-  //     )
-  //     output.push({ ...entry, prijs: '-1' })
-  //   }
-  // }
+  await queue.onEmpty()
   await browser.close()
-
-  require('fs').writeFileSync('./data2.json', JSON.stringify(output), {
-    encoding: 'utf8',
-  })
+  // require('fs').writeFileSync('./data2.json', JSON.stringify(output), {
+  //   encoding: 'utf8',
+  // })
+  console.log('done')
 })()
 
 const addPrice = async (entry, browser) => {
-  const years =
-    (new Date() -
-      new Date(
-        `${entry.Datumeerstetoelating.slice(
-          0,
-          4,
-        )}-${entry.Datumeerstetoelating.slice(
-          4,
-          6,
-        )}-${entry.Datumeerstetoelating.slice(6)}`,
-      )) /
-    (1000 * 60 * 60 * 24 * 365)
-
-  if (years >= 15) {
-    console.log(`Skipping ${entry.Kenteken}, it's too old (${years} old)`)
-    return { ...entry, prijs: '-1' }
-  }
-  const kilometers = KM_PER_YEAR * years
-  const page = await browser.newPage()
+  const kilometers = KM_PER_YEAR * entry.years
+  let page
+  let price
   try {
-    const price = await getPrice(
+    page = await browser.newPage()
+  } catch (e) {
+    console.error('could not create page')
+    console.error(e)
+    await page.close()
+    return { ...entry, prijs: '-3' }
+  }
+  try {
+    price = await getPrice(
       entry.Kenteken,
       kilometers.toString().split('.')[0],
       page,
     )
-    console.log(`prijs voor ${entry.Kenteken}: ${price}`)
-    console.log(`progress: ${(output.length / input.length) * 100}%`)
-    page.close()
-    return { ...entry, prijs: price }
   } catch (e) {
     console.error(
       `failed to get price for ${entry.Kenteken}, kilometers: ${
         kilometers.toString().split('.')[0]
       }`,
     )
-    page.close()
+    await page.close()
     return { ...entry, prijs: '-1' }
   }
+  console.log(`prijs voor ${entry.Kenteken}: ${price}`)
+  await page.close()
+  price = price.slice(2)
+  price = price.replace('.', ',')
+  return { ...entry, prijs: price }
 }
